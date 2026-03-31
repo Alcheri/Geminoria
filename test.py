92ea6493d299c5384279e7db08187f41001e9448
Alcheri <barry.suridge@gmail.com>
2026-03-31 22:05:12 +1100

    Initial commit

---
+++ test.py
@@ -0,0 +1,13 @@
+###
+# Copyright © MMXXV, Barry Suridge
+# All rights reserved.
+###
+
+from supybot.test import PluginTestCase
+
+
+class GeminoriaTestCase(PluginTestCase):
+    plugins = ("Geminoria",)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
